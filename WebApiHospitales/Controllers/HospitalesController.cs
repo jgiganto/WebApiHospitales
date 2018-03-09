a5879aec90e45dc2d054f948101adb839f9c021d
jgiganto <jgiganto1@hotmail.com>
2018-03-09 14:26:37 +0100

    d

---
+++ WebApiHospitales/Controllers/HospitalesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiHospitales.Models;
+
+namespace WebApiHospitales.Controllers
+{
+    public class HospitalesController : ApiController
+    {
+        ContextoHospital contexto;
+        public HospitalesController HospitalesController()
+        {
+
+        }
+    }
+}
---
+++ WebApiHospitales/Models/ModeloHospital.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiHospitales.Models
+{
+    public class ModeloHospital
+    {
+        ContextoHospital contexto;
+        public ModeloHospital()
+        {
+            contexto = new ContextoHospital();
+        }
+
+        public List<HOSPITAL> GetHospital()
+        {
+            var consulta = from datos in contexto.HOSPITAL
+                           select datos;
+            return consulta.ToList();
+        }
+
+    }
+}
